8d349edf857cc8acfb83bf4c44d05ed474c4a61b
bargho <bargho@umich.edu>
2014-10-21 20:16:15 -0400

    10/21 commit
    Added base about page, removed fluff .js stuff

--- ProtoDB/ProtoDB/urls.py
+++ ProtoDB/ProtoDB/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ProtoDB.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/$', 'databasemodels.views.home'),
     url(r'^register/$', 'databasemodels.views.register'),
@@ -18,4 +14,5 @@ urlpatterns = patterns('',
     url(r'^protocol_list/$', 'databasemodels.views.protocol_list'),
     url(r'^view/(?P<slug>[^\.]+).html', 'databasemodels.views.protocol',name='protocol'),
     (r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html")),
 )
